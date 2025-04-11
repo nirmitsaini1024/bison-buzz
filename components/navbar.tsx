7e1585553c55893820cc7728a4b100d2f7af3687
nirmitsaini1024 <nirmitsaini24@gmail.com>
2025-04-11 09:55:51 +0530

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -235,15 +235,6 @@ const Navbar = () => {
           </div>
         </div>
       )}
-      
-      {/* Bottom Mobile Menu */}
-      <div className="lg:hidden fixed bottom-0 left-0 right-0 bg-gray-800 flex justify-center py-3 z-50">
-        <button className="text-white">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
-        </button>
-      </div>
     </nav>
   );
 };
